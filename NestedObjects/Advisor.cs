cec1d8759a68e914b022c873b49820cd9e51d087
Kris <dauthi66@gmail.com>
2022-01-17 13:43:42 -0800

    Created student class, and nested advisor class.

---
+++ NestedObjects/Advisor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{   
+    /// <summary>
+    /// Represents a college advisor
+    /// </summary>
+    public class Advisor
+    {   
+        /// <summary>
+        /// Legal first and last name
+        /// </summary>
+        public string FullName {  get; set; }
+        /// <summary>
+        /// Advisor's work email
+        /// </summary>
+        public string Email {  get; set; }
+        /// <summary>
+        /// Building and room number where their office is located
+        /// </summary>
+        public string OfficeLocation {  get; set; }
+    }
+}
---
+++ NestedObjects/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Reprepresents an individual college student
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// Legal first name
+        /// </summary>
+        public string FirstName {  get; set; }
+        /// <summary>
+        /// Legal last name
+        /// </summary>
+        public string LastName {  get; set; }
+        /// <summary>
+        /// The assigned school email 
+        /// </summary>
+        public string SchoolEmail {  get; set; }
+        /// <summary>
+        /// Current advisor assigned to student
+        /// </summary>
+        public Advisor AssignedAdvisor {  get; set; }
+    }
+}
+ 
